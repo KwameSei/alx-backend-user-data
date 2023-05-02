ea58807342ee269c990aa0f4f96eab550d0f1195
KwameSei <nathanielosei92@gmail.com>
2023-05-02 18:28:37 +0000

    Created 401 error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def error_401() -> str:
+    """ GET /api/v1/unauthorized
+      Return: unauthorized 401 status code
+    """
+    abort(401)
