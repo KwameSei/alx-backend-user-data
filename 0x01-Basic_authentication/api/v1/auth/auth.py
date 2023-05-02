726c42568aa3755fb1a097e1cc67e26698b3917b
KwameSei <nathanielosei92@gmail.com>
2023-05-02 20:04:45 +0000

    Excluded paths methods completed

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,7 +16,29 @@ class Auth:
           This method requires authentication for all routes except
           /status and /api/v1/unauthorized and /api/v1/forbidden
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            # Excluded paths is empty, so require auth for all paths
+            # Excluded paths include /api/v1/status, /api/v1/unauthorized,
+            return True
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):  # Add trailing slash if not
+                # excluded_path += '/'
+                excluded_path = excluded_path[:-1]  # Remove trailing slash
+            if path == excluded_path or path.startswith(excluded_path):
+                # Path is excluded, so do not require auth
+                return False
+
+        return True
+
+        # if path[-1] != '/':
+        #     path += '/'
+        # if path in excluded_paths:
+        #     return False
+        # return True
 
     def authorization_header(self, request=None) -> str:
         """
