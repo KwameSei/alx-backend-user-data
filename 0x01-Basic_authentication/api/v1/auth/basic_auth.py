a884efdecb6ddf4c34b5c6be3845ab5ee00e35a6
KwameSei <nathanielosei92@gmail.com>
2023-05-03 05:04:56 +0000

    Addesd authorization header to basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,15 @@ class BasicAuth(Auth):
     """
       Class for Basic Authentication
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+          Method that returns Base64 part of the Authorization header.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):   # str only
+            return None
+        if not authorization_header.startswith('Basic '):   # Basic space
+            return None
+        return authorization_header[6:]   # 6 is len('Basic ')
