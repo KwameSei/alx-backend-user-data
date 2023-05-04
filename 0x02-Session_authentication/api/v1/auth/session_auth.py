2810d66d9846926c6b100b593b258ddb4dd8d36d
KwameSei <nathanielosei92@gmail.com>
2023-05-04 04:55:13 +0000

    Session method created

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,25 @@
   Implementation of API session authentication.
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
       Class for Session Authentication
     """
-    pass
+    user_id_by_session_id = {}   # Dictionary of session_id: user_id
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+          Implement a Session ID for a user_id
+        """
+        if not user_id or isinstance(user_id) != str:  # if user_id is None
+            return None
+
+        session_id = str(uuid4())   # create session_id
+        if not session_id:
+            return None
+
+        self.user_id_by_session_id[session_id] = user_id   # add to dictionary
+        return session_id
