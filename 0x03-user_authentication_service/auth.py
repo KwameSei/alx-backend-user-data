6a44298bd787c797d3daf517f81f4b7e6179c419
KwameSei <nathanielosei92@gmail.com>
2023-05-11 18:14:31 +0000

    User registration implemented

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python3
 """ Creating auth module """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
     """ Method that accepts password argument and returns bytes """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())  # Returns bytes
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Method that registers and returns a new user """
+
+        try:
+            user = self._db.find_user_by(email=email)  # Find user by email
+            if user:   # If user exists, raise ValueError
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)   # Hash password
+            user_add = self._db.add_user(email=email,
+                                         hashed_password=hashed_password)
+
+            return user_add
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 """DB module
 """
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from contextlib import contextmanager
+from typing import Union
 from user import Base, User
+
+
+@contextmanager
+def session_scope(Session):
+    """Provide a transactional scope around a series of operations."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except sqlalchemy.exc.SQLAlchemyError:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 class DB:
@@ -20,30 +37,36 @@ class DB:
         self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
-        self.__session = None
+        self.__session = sessionmaker(bind=self._engine)
 
     @property
     def _session(self) -> Session:
         """Memoized session object
         """
         if self.__session is None:
-            DBSession = sessionmaker(bind=self._engine)
+            DBSession = sessionmaker(bind=self._engine, autocommit=True,
+                                     autoflush=True)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Method that adds user to the database """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)  # Adds new User record to the database
-        self._session.commit()   # Commits all changes
+        with session_scope(self._session) as session:
+            user = User(email=email, hashed_password=hashed_password)
+            session.add(user)  # Adds new User record to the database
+            session.commit()   # Commits all changes
         return user  # Returns the new User object
 
-    def find_user_by(self, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs: dict) -> Union[User, None]:
         """ Method that returns user found in database """
         if kwargs is None:
             raise InvalidRequestError
 
-        user = self.__session.query(User).filter_by(**kwargs).first()
+        if self.__session is None:
+            self._session  # create new session if one not already initialized
+
+        with session_scope(self._session) as session:
+            user = session.query(User).filter_by(**kwargs).first()
         if user is None:    # If no user found, raise NoResultFound
             raise NoResultFound
 
